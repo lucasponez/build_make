neerajregmi10/android_build

Flashing Script at Recovery

tools/releasetools/ota_from_target_files.py
@@ -962,6 +962,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # Stage 3/3: Make changes.
     script.Comment("Stage 3/3")
 
+  # Inform User
+  script.Print("BackupTool at Work");
+
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
@@ -980,31 +983,53 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if OPTIONS.backuptool:
     script.RunBackup("backup", sysmount)
 
+  script.Print("BackupTool Work Done");
+
   system_progress = 0.75
+
+  script.Print("**************************************");
+  script.Print("**  				    **");
+  script.Print("**  				    **");
+  script.Print("**  	      LEGIONOS		    **");
+  script.Print("**  				    **");
+  script.Print("**  				    **");
+  script.Print("**                                  **");
+  script.Print("**************************************");
+  script.Print("**  by Dr. Raj Kale (*TeamLegion)   **");
+  script.Print("**         Android Q                **");
+  script.Print("**************************************");
+
+  legionversion = target_info.GetBuildProp("ro.legion.display.version")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  buildtype = target_info.GetBuildProp("ro.build.type")
+  buildid = target_info.GetBuildProp("ro.build.id")
+  buildday = target_info.GetBuildProp("ro.build.date")
+  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.name")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+
+  script.Print("***********************************************");
+  script.Print(" ROM version      : %s"%(legionversion));
+  script.Print("");
+  script.Print(" Android version  : %s"%(androidver));
+  script.Print("");
+  script.Print(" Security patch   : %s"%(securep));
+  script.Print("");
+  script.Print(" Build date       : %s"%(buildday));
+  script.Print("");
+  script.Print(" Build type       : %s"%(buildtype));
+  script.Print("");
+  script.Print(" Device           : %s"%(device));
+  script.Print("");
+  script.Print(" Manufacturer     : %s"%(manufacturer));
+  script.Print("***********************************************");
 
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
     system_progress -= 0.1
 
-  script.Print("=====================================================");
-  script.Print(" _      ______ _____ _____ ____  _   _  ____   ____ _");
-  script.Print("| |    |  ____/ ____|_   _/ __ \| \ | |/ __ \ / ____|");
-  script.Print("| |    | |__ | |  __  | || |  | |  \| | |  | | (___  ");
-  script.Print("| |    |  __|| | |_ | | || |  | | . ` | |  | |\___ \ ");
-  script.Print("| |____| |___| |__| |_| || |__| | |\  | |__| |____) |");
-  script.Print("|______|______\_____|_____\____/|_| \_|\____/|_____/ ");
-  script.Print("						                                         ");
-  script.Print("                    By RAJ KALE                      ");
-  script.Print("          Thanks For Flashing LEGIONOS Q             ");
-  script.Print("                                                     ");
-  script.Print("=====================================================");
-  script.Print(" Android version: %s"%(android_version));
-  script.Print(" Build id: %s"%(build_id));
-  script.Print(" Build date: %s"%(build_date));
-  script.Print(" Security patch: %s"%(security_patch));
-  script.Print(" Device: %s"%(device));
-  script.Print("=====================================================");
+  script.ShowProgress(system_progress, 0)
 
   def GetBlockDifference(partition):
     # Full OTA is done as an "incremental" against an empty source image. This
@@ -1065,6 +1090,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
+
+  script.Print("Welcome to LegionS : A Custom ROM by Dr. Raj Kale");
 
   if OPTIONS.extra_script is not None:
     script.AppendExtra(OPTIONS.extra_script)

lucasponez/build_make

tools/releasetools/ota_from_target_files.py
@@ -814,6 +814,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # Stage 3/3: Make changes.
     script.Comment("Stage 3/3")
 
+  # Inform User
+  script.Print("BackupTool at Work");
+
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
@@ -830,33 +833,53 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.RunBackup("backup")
     script.Unmount("/system")
 
+  script.Print("BackupTool Work Done");
+
   # All other partitions as well as the data wipe use 10% of the progress, and
   # the update of the system partition takes the remaining progress.
   system_progress = 0.9 - (len(block_diff_dict) - 1) * 0.1
+
+  script.Print("**************************************");
+  script.Print("**  				    **");
+  script.Print("**  				    **");
+  script.Print("**  	      LEGIONOS		    **");
+  script.Print("**  				    **");
+  script.Print("**  				    **");
+  script.Print("**                                  **");
+  script.Print("**************************************");
+  script.Print("**  by Dr. Raj Kale (*TeamLegion)   **");
+  script.Print("**         Android R                **");
+  script.Print("**************************************");
+
+  legionversion = target_info.GetBuildProp("ro.legion.display.version")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  buildtype = target_info.GetBuildProp("ro.build.type")
+  buildid = target_info.GetBuildProp("ro.build.id")
+  buildday = target_info.GetBuildProp("ro.build.date")
+  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.name")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+
+  script.Print("***********************************************");
+  script.Print(" ROM version      : %s"%(legionversion));
+  script.Print("");
+  script.Print(" Android version  : %s"%(androidver));
+  script.Print("");
+  script.Print(" Security patch   : %s"%(securep));
+  script.Print("");
+  script.Print(" Build date       : %s"%(buildday));
+  script.Print("");
+  script.Print(" Build type       : %s"%(buildtype));
+  script.Print("");
+  script.Print(" Device           : %s"%(device));
+  script.Print("");
+  script.Print(" Manufacturer     : %s"%(manufacturer));
+  script.Print("***********************************************");
 
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   progress_dict = {partition: 0.1 for partition in block_diff_dict}
   progress_dict["system"] = system_progress
-
-  script.Print("=====================================================");
-  script.Print(" _      ______ _____ _____ ____  _   _  ____   ____ _");
-  script.Print("| |    |  ____/ ____|_   _/ __ \| \ | |/ __ \ / ____|");
-  script.Print("| |    | |__ | |  __  | || |  | |  \| | |  | | (___  ");
-  script.Print("| |    |  __|| | |_ | | || |  | | . ` | |  | |\___ \ ");
-  script.Print("| |____| |___| |__| |_| || |__| | |\  | |__| |____) |");
-  script.Print("|______|______\_____|_____\____/|_| \_|\____/|_____/ ");
-  script.Print("						                                         ");
-  script.Print("                    By RAJ KALE                      ");
-  script.Print("          Thanks For Flashing LEGIONOS R             ");
-  script.Print("                                                     ");
-  script.Print("=====================================================");
-  script.Print(" Android version: %s"%(android_version));
-  script.Print(" Build id: %s"%(build_id));
-  script.Print(" Build date: %s"%(build_date));
-  script.Print(" Security patch: %s"%(security_patch));
-  script.Print(" Device: %s"%(device));
-  script.Print("=====================================================");
 
   if target_info.get('use_dynamic_partitions') == "true":
     # Use empty source_info_dict to indicate that all partitions / groups must
@@ -890,6 +913,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.ShowProgress(0.1, 10)
   device_specific.FullOTA_InstallEnd()
+
+  script.Print("Welcome to LegionS : A Custom ROM by Dr. Raj Kale");
 
   if OPTIONS.extra_script is not None:
     script.AppendExtra(OPTIONS.extra_script)